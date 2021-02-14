aa5054c2865a774370f773dd0b757254fd7d89b1
Benedek Halasi <b@pop-os.localdomain>
2021-02-14 13:43:56 +0100

    rn docs to dist so cd pipeline can work again

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
-  generate: {
-    dir: "docs"
-  },
   
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
