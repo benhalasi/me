2dfc1171422e01deaa59ca3b0937154f1b076a38
Benedek Halasi <b@pop-os.localdomain>
2021-02-14 13:09:40 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-black w-screen h-screen flex items-center justify-center">
+    <span class="text-gray-200 text-9xl">
+      Hi there.
+    </span>
+  </div>
+</template>
