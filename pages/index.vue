09d9245f926c2f120baf7953c19598d22f4fb119
Benedek Halasi <halasibenedek.p@gmail.com>
2021-02-28 07:42:04 +0100

    final design without content

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,116 @@
 <template>
-  <div class="bg-black w-screen h-screen flex items-center justify-center">
-    <span class="text-gray-200 text-9xl">
-      Hi there.
-    </span>
+  <div class="w-full h-screen bg-black font-body text-gray-200 p-px">
+    <div class="flex flex-col min-h-full w-full xl:max-w-7xl 2xl:max-w-7xl mx-auto ">
+      <header class="flex-grow-0 flex flex-wrap sm:flex-nowrap justify-between">
+        <div class="font-display text-9xl uppercase border-brutal">
+          bh
+        </div>
+        <div class="flex-grow border-brutal"><!-- placeholder --></div>
+        <div class="flex flex-col justify-end sm:max-w-prose p-2 border-brutal text-sm">
+          <p>
+            I'm in <sup>tm</sup>Java but still don't think JavaScript is the worst thing to ever happend to humanity.
+            I use <span>Optionals</span> instead of <span>nulls</span> and <span>Streams</span> over <span>fors</span>.
+          </p>
+          <p>
+            This <span class="line-through">is</span> will be the collection of stuff I made and does not make me cry.
+          </p>
+        </div>
+      </header>
+      <main class="flex flex-wrap justify-start border-brutal">
+
+        <div class="article">
+          <div class="w-full text-xl p-2">
+            <span class="uppercase font-display pr-1">POC</span>-<span class="lowercase pl-1">mock content</span>
+          </div>
+          <div class="flex-grow p-2 border-b border-t border-gray-200">
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+          <div class="w-full text-sm p-2 flex justify-end">
+            <a class="lowercase hover:underline focus:underline" href="#">Read more @GitHub.com</a>
+          </div>
+        </div>
+
+        <div class="article">
+          <div class="w-full text-xl p-2">
+            <span class="uppercase font-display pr-1">POC</span>-<span class="lowercase pl-1">mock content</span>
+          </div>
+          <div class="flex-grow p-2 border-b border-t border-gray-200">
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+          <div class="w-full text-sm p-2 flex justify-end">
+            <a class="lowercase hover:underline focus:underline" href="#">Read more @GitHub.com</a>
+          </div>
+        </div>
+
+        <div class="article">
+          <div class="w-full text-xl p-2">
+            <span class="uppercase font-display pr-1">FD</span>-<span class="lowercase pl-1">mock content</span>
+          </div>
+          <div class="flex-grow p-2 border-b border-t border-gray-200">
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+          <div class="w-full text-sm p-2 flex justify-end">
+            <a class="lowercase hover:underline focus:underline" href="#">Read more @GitHub.com</a>
+          </div>
+        </div>
+
+        <div class="article">
+          <div class="w-full text-xl p-2">
+            <span class="uppercase font-display pr-1">POC</span>-<span class="lowercase pl-1">mock content</span>
+          </div>
+          <div class="flex-grow p-2 border-b border-t border-gray-200">
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+          <div class="w-full text-sm p-2 flex justify-end">
+            <a class="lowercase hover:underline focus:underline" href="#">Read more @GitHub.com</a>
+          </div>
+        </div>
+
+        <div class="article">
+          <div class="w-full text-xl p-2">
+            <span class="uppercase font-display pr-1">SP</span>-<span class="lowercase pl-1">mock content</span>
+          </div>
+          <div class="flex-grow p-2 border-b border-t border-gray-200">
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+          <div class="w-full text-sm p-2 flex justify-end">
+            <a class="lowercase hover:underline focus:underline" href="#">Read more @GitHub.com</a>
+          </div>
+        </div>
+
+      </main>
+
+      <div class="flex-grow w-full border-brutal"><!-- placeholder --></div>
+
+      <footer class="flex-grow-0 flex flex-wrap justify-between">
+        <div class="font-display text-9xl uppercase border-brutal">
+          bh
+        </div>
+        <div class="flex-grow border-brutal"><!-- placeholder --></div>
+        <div class="flex flex-col justify-end flex-nowrap w-full sm:max-w-max p-2 border-brutal text-sm align-bottom">
+          <p>
+            // don't forget to add contact info
+          </p>
+          <!-- <span class="flex-grow-0 flex-shrink truncate pr-1">Contact me via the interweb:</span>
+          <span class="hover:underline focus:underline">bphalasi@gmail.com</span> -->
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,23 @@ module.exports = {
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
-  ],  darkMode: false, // or 'media' or 'class'
+  ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'display': ["'Major Mono Display'", 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "'Liberation Mono'", "'Courier New'", 'monospace'],
+      'body': ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "'Segoe UI'", 'Roboto', "'Helvetica Neue'", 'Arial', "'Noto Sans'", 'sans-serif', "'Apple Color Emoji'", "'Segoe UI Emoji'", "'Segoe UI Symbol'", "'Noto Color Emoji'"],
+    },
+    outline: {
+      DEFAULT: ['2px solid transparent', '-1px'],
+      brutal: ['2px solid white', '-1px']
+    },
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      outline: ['hover', 'focus-within', 'focus'],
+    }
   },
   plugins: [],
 }
