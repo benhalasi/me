308b5c9a9f5a33573f0047ca762802cce8059d0c
Benedek Halasi <t0h3ow7t2@mozmail.com>
2022-12-19 12:37:48 +0100

    add user-presentable-logger article

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -192,6 +192,26 @@ export default component$(() => {
           an <em>'Interactive UI fragment'</em> on multiple pages in an MVC app.
         </p>
       </Article>
+      <Article
+        name="User Presentable Logger"
+        type="XP"
+        links={{
+          source: {
+            GitHub: "https://github.com/benhalasi/user-presentable-logger",
+          },
+        }}
+      >
+        <p>
+          Logger library for presenting the results, issues of batch and
+          background jobs and events to the user.
+        </p>
+        <p>
+          This project is built with a 'managment' type application in mind
+          where users have to handle multiple types of processes in a way that a
+          dashboard with ongoing jobs, unresolved issues, unexpected or expected
+          events is neccessary.
+        </p>
+      </Article>
     </main>
   );
 });
