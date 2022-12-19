678f107c0be2d19d49ba1f85deb8520257cebf0b
Benedek Halasi <t0h3ow7t2@mozmail.com>
2022-12-19 12:30:30 +0100

    add herocions-x-qwik article

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -126,6 +126,34 @@ export default component$(() => {
         </div>
       </Article>
       <Article
+        name="Heroicons for Qwik"
+        type="SP"
+        links={{
+          source: {
+            GitHub: "https://github.com/benhalasi/heroicons-x-qwik",
+            Npm: "https://www.npmjs.com/package/heroicons-x-qwik",
+          },
+        }}
+      >
+        <p>
+          <a
+            href="https://heroicons.com/"
+            target="heroicons"
+            rel="noopener noreferrer"
+          >
+            Heroicons
+          </a>{" "}
+          is an icon library for vue and react. This project ports it to{" "}
+          <a
+            href="https://qwik.builder.io/"
+            target="qwik"
+            rel="noopener noreferrer"
+          >
+            Qwik
+          </a>
+        </p>
+      </Article>
+      <Article
         name="Pastry Shop"
         type="POC"
         links={{
